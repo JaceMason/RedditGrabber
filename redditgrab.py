03a97dfb52b75dc4e6da10f5161ae517fa5f363a
Jace <JaceMason@msn.com>
2022-04-11 20:15:54 -0700

    Turned the RedditGrabber into a class
    Going to use it in another form and want it to be a bit more modular.
    Functionality is mostly identical to what it was before.
    
    -Fixed an issue where it could grab more posts than requested. Wasn't
    checking if we hit our cap while we were recording.

--- redditgrab.py
+++ redditgrab.py
@@ -6,45 +6,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-
-if __name__ == "__main__":
-
-    numPostsToGrab = 30
+class RedditGrabber:
     options = Options()
     #options.headless = True
-    driver = webdriver.Firefox(options = options)
 
     redditPostClass = "_1oQyIsiPHYt6nx7VOmd1sz"
     redditHdrClass = "_eYtD2XCVieq6emjKBH3m"
     subredditClass = "_2mHuuvyV9doV3zwbZPtIPG"
     adClass = "_2oEYZXchPfHwcf9mTMGMg8"
     
-    driver.get("https://www.reddit.com")
-
-    
-    skipAds = True
-    postsOnScreen = 0
-    lastPost = 0
-    textPosts = []
-    while postsOnScreen < numPostsToGrab:
-        posts = driver.find_elements_by_class_name(redditPostClass)
-        while lastPost < len(posts):
-            #To deal with infinite scrolling, we need to scroll to the new posts as they appear (or to the bottom)
-            driver.execute_script("arguments[0].scrollIntoView();", posts[lastPost])
-
-            #Do not include ads in the collected posts if requested.
-            if not posts[lastPost].find_elements_by_class_name(adClass) and skipAds:
-                headline = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, redditHdrClass)))
-                subreddit = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, subredditClass)))            
-                textPosts.append((headline.text, subreddit.text))
-
-            lastPost += 1
+    def get_reddit_posts(self, postAmount, skipAds=True):
+        driver = webdriver.Firefox(options = self.options)
+        driver.get("https://www.reddit.com")
+        
+        postsOnScreen = 0
+        lastPost = 0
+        textPosts = []
+        while postsOnScreen < postAmount:
+            posts = driver.find_elements_by_class_name(self.redditPostClass)
+            while lastPost < len(posts) and len(textPosts) < postAmount:
+                #To deal with infinite scrolling, we need to scroll to the new posts as they appear (or to the bottom)
+                driver.execute_script("arguments[0].scrollIntoView();", posts[lastPost])
                 
-        postsOnScreen = len(textPosts)
-        WebDriverWait(posts[-1], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, redditHdrClass)))
+                #Do not include ads in the collected posts if requested.
+                if not posts[lastPost].find_elements_by_class_name(self.adClass) and skipAds:
+                    headline = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
+                    subreddit = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.subredditClass)))
+                    textPosts.append((headline.text, subreddit.text))
 
-    for text in textPosts:
-        print(text);
-    
-    driver.quit()
+                lastPost += 1
+                
+            postsOnScreen = len(textPosts)
+            WebDriverWait(posts[-1], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
+
+        for text in textPosts:
+            print(text)
+        driver.quit()
+        return textPosts
+
+if __name__ == "__main__":
+    blah = RedditGrabber()
+    blah.get_reddit_posts(5)
