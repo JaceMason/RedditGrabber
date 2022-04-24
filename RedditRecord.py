e3412ad9865d520d33a6f8ec2fdb4d85bf4b8af7
Jace <JaceMason@msn.com>
2022-04-24 15:05:57 -0700

    Now using tabs for indenting (No logical changes)
    I would have preferred to use tabs from the beginning, but it took me a
    bit to get emacs to behave properly.

--- RedditRecord.py
+++ RedditRecord.py
@@ -3,93 +3,93 @@ from datetime import datetime, timezone
 import os
 
 class RedditRecorder:
-    def raise_type_error(self, message):
-        raise TypeError(message + " Data must be in the form of [(<int>, <str>, <str>), ...]")
-    
-    def record_posts(self, dataTupleList):
-        '''
-        Incoming data must be a list of tuples with the following structure:
-        
-        (<int>, <str>, <str>)
-        (postRank, subreddit, headline)
-        
-        Check for this.
-        '''
-        #must be list
-        if not isinstance(dataTupleList, list):
-            self.raise_type_error("Incoming data was not a list.")
-        
-        for listEntryIndex, listEntry in enumerate(dataTupleList):
-            #All list entries must be tuples
-            if not isinstance(listEntry, tuple):
-                    self.raise_type_error("List entry %d must be tuple." % listEntryIndex)
-            #tuples must have length 3
-            if len(listEntry) != 3:
-                self.raise_type_error("List entry %d was of size %d." % (listEntryIndex, len(listEntry)))
-            #must be in the form (<int>, <str>, <str>)
-            if not isinstance(listEntry[0], int):
-                    self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 0))
-            if not isinstance(listEntry[1], str):
-                    self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 1))
-            if not isinstance(listEntry[2], str):
-                    self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 2))
-        
-        database = sqlite3.connect('redditlog.db')
-        
-        cur = database.cursor()
-        
-        #Initialize the database
-        cur.execute("PRAGMA foreign_keys = ON;")
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS polls(
-                poll_id INTEGER PRIMARY KEY,
-                poll_time REAL)
-        ''')
-        
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS posts(
-                post_poll INTEGER,
-                post_rank INTEGER,
-                subreddit TEXT,
-                headline TEXT,
-                FOREIGN KEY(post_poll) REFERENCES polls(poll_id))
-        ''')
-        
-        #insert some values
-        cur.execute('''
-            INSERT INTO polls (poll_time)
-            VALUES(%f)'''
-            % datetime.utcnow().timestamp()
-        )
-        
-        newRow = cur.lastrowid
-        dataList = [(newRow,) + tupl for tupl in dataTupleList]
-        
-        cur.executemany("INSERT INTO posts VALUES (?, ?, ?, ?)", dataList)
-        
-        database.commit()
-        database.close()
+	def raise_type_error(self, message):
+		raise TypeError(message + " Data must be in the form of [(<int>, <str>, <str>), ...]")
+	
+	def record_posts(self, dataTupleList):
+		'''
+		Incoming data must be a list of tuples with the following structure:
+		
+		(<int>, <str>, <str>)
+		(postRank, subreddit, headline)
+		
+		Check for this.
+		'''
+		#must be list
+		if not isinstance(dataTupleList, list):
+			self.raise_type_error("Incoming data was not a list.")
+		
+		for listEntryIndex, listEntry in enumerate(dataTupleList):
+			#All list entries must be tuples
+			if not isinstance(listEntry, tuple):
+				self.raise_type_error("List entry %d must be tuple." % listEntryIndex)
+			#tuples must have length 3
+			if len(listEntry) != 3:
+				self.raise_type_error("List entry %d was of size %d." % (listEntryIndex, len(listEntry)))
+			#must be in the form (<int>, <str>, <str>)
+			if not isinstance(listEntry[0], int):
+				self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 0))
+			if not isinstance(listEntry[1], str):
+				self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 1))
+			if not isinstance(listEntry[2], str):
+				self.raise_type_error("List entry %d, tuple entry %d." % (listEntryIndex, 2))
+		
+		database = sqlite3.connect('redditlog.db')
+		
+		cur = database.cursor()
+		
+		#Initialize the database
+		cur.execute("PRAGMA foreign_keys = ON;")
+		cur.execute('''
+			CREATE TABLE IF NOT EXISTS polls(
+				poll_id INTEGER PRIMARY KEY,
+				poll_time REAL)
+		''')
+		
+		cur.execute('''
+			CREATE TABLE IF NOT EXISTS posts(
+				post_poll INTEGER,
+				post_rank INTEGER,
+				subreddit TEXT,
+				headline TEXT,
+				FOREIGN KEY(post_poll) REFERENCES polls(poll_id))
+		''')
+		
+		#insert some values
+		cur.execute('''
+			INSERT INTO polls (poll_time)
+			VALUES(%f)'''
+			% datetime.utcnow().timestamp()
+		)
+		
+		newRow = cur.lastrowid
+		dataList = [(newRow,) + tupl for tupl in dataTupleList]
+		
+		cur.executemany("INSERT INTO posts VALUES (?, ?, ?, ?)", dataList)
+		
+		database.commit()
+		database.close()
 
 if __name__ == "__main__":
-    blah = RedditRecorder()
-    
-    goodData = [(1, "r/test", "Headline"), (2, "r/exam", "Highlight"), (3, "r/assessment", "Summary")]
-    blah.record_posts(goodData)
+	blah = RedditRecorder()
+	
+	goodData = [(1, "r/test", "Headline"), (2, "r/exam", "Highlight"), (3, "r/assessment", "Summary")]
+	blah.record_posts(goodData)
 
-    #couple quick tests for the type errors.
-    print("Testing for poorly structured input data:")
-    badDataList = [
-        "Hello",
-        ["Hello"],
-        [("Hello")],
-        [("Hello", 1, "Uh.")],
-        [(1, 2, "Hello")],
-        [(1, "Hello", 2.2)],
-        [(1, "r/Hello", "2.2"), (3, "r/Hi", 1)]
-    ]
-    for i, badData in enumerate(badDataList):
-        try:
-            blah.record_posts(badData)
-            print("Uh oh. Test %d failed to fail" % i)
-        except TypeError:
-            print("Test %d failed successfully" % i)
+	#couple quick tests for the type errors.
+	print("Testing for poorly structured input data:")
+	badDataList = [
+		"Hello",
+		["Hello"],
+		[("Hello")],
+		[("Hello", 1, "Uh.")],
+		[(1, 2, "Hello")],
+		[(1, "Hello", 2.2)],
+		[(1, "r/Hello", "2.2"), (3, "r/Hi", 1)]
+	]
+	for i, badData in enumerate(badDataList):
+		try:
+			blah.record_posts(badData)
+			print("Uh oh. Test %d failed to fail" % i)
+		except TypeError:
+		    print("Test %d failed successfully" % i)
--- redditgrab.py
+++ redditgrab.py
@@ -7,43 +7,43 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 
 class RedditGrabber:
-    options = Options()
-    #options.headless = True
+	options = Options()
+	#options.headless = True
 
-    redditPostClass = "_1oQyIsiPHYt6nx7VOmd1sz"
-    redditHdrClass = "_eYtD2XCVieq6emjKBH3m"
-    subredditClass = "_2mHuuvyV9doV3zwbZPtIPG"
-    adClass = "_2oEYZXchPfHwcf9mTMGMg8"
-    
-    def get_reddit_posts(self, postAmount, skipAds=True):
-        driver = webdriver.Firefox(options = self.options)
-        driver.get("https://www.reddit.com")
-        
-        postsOnScreen = 0
-        lastPost = 0
-        textPosts = []
-        while postsOnScreen < postAmount:
-            posts = driver.find_elements_by_class_name(self.redditPostClass)
-            while lastPost < len(posts) and len(textPosts) < postAmount:
-                #To deal with infinite scrolling, we need to scroll to the new posts as they appear (or to the bottom)
-                driver.execute_script("arguments[0].scrollIntoView();", posts[lastPost])
-                
-                #Do not include ads in the collected posts if requested.
-                if not posts[lastPost].find_elements_by_class_name(self.adClass) and skipAds:
-                    headline = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
-                    subreddit = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.subredditClass)))
-                    textPosts.append((len(textPosts)+1, subreddit.text, headline.text))
+	redditPostClass = "_1oQyIsiPHYt6nx7VOmd1sz"
+	redditHdrClass = "_eYtD2XCVieq6emjKBH3m"
+	subredditClass = "_2mHuuvyV9doV3zwbZPtIPG"
+	adClass = "_2oEYZXchPfHwcf9mTMGMg8"
+	
+	def get_reddit_posts(self, postAmount, skipAds=True):
+		driver = webdriver.Firefox(options = self.options)
+		driver.get("https://www.reddit.com")
+		
+		postsOnScreen = 0
+		lastPost = 0
+		textPosts = []
+		while postsOnScreen < postAmount:
+			posts = driver.find_elements_by_class_name(self.redditPostClass)
+			while lastPost < len(posts) and len(textPosts) < postAmount:
+				#To deal with infinite scrolling, we need to scroll to the new posts as they appear (or to the bottom)
+				driver.execute_script("arguments[0].scrollIntoView();", posts[lastPost])
+				
+				#Do not include ads in the collected posts if requested.
+				if not posts[lastPost].find_elements_by_class_name(self.adClass) and skipAds:
+					headline = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
+					subreddit = WebDriverWait(posts[lastPost], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.subredditClass)))
+					textPosts.append((len(textPosts)+1, subreddit.text, headline.text))
 
-                lastPost += 1
-                
-            postsOnScreen = len(textPosts)
-            WebDriverWait(posts[-1], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
+				lastPost += 1
+				
+			postsOnScreen = len(textPosts)
+			WebDriverWait(posts[-1], 20).until(EC.visibility_of_element_located((By.CLASS_NAME, self.redditHdrClass)))
 
-        driver.quit()
-        return textPosts
+		driver.quit()
+		return textPosts
 
 if __name__ == "__main__":
-    blah = RedditGrabber()
-    posts = blah.get_reddit_posts(25)
-    for post in posts:
-        print(post)
+	blah = RedditGrabber()
+	posts = blah.get_reddit_posts(25)
+	for post in posts:
+		print(post)
